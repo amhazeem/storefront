5275a9c8031cdaa714bd3038b0953ac9562a8730
Animashaun Abdul Azeem <azeemanimashaun@gmail.com>
2020-11-29 09:19:58 +0100

    Created books

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from books.models import Book
+
+
+class BookTest(TestCase):
+
+    def create_book(self, title="Book 1", description="This is a sample book written by XY"):
+        return Book.objects.create(title=title, description=description)
+
+    def test_book_creation(self):
+        book = self.create_book()
+        self.assertTrue(isinstance(book, Book))
+        self.assertEqual(book.__str__(), book.title)
+
